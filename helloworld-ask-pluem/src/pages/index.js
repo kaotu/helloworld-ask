ed434b52bf403b6faad1a5e73e6ddbc6f180014f
ปลื้ม <papop.ru@mail.kmutt.ac.th>
2018-06-18 21:57:01 +0700

    [change] เพิ่ม form

--- helloworld-ask-pluem/src/pages/index.js
+++ helloworld-ask-pluem/src/pages/index.js
@@ -3,10 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>ASK 3.0</h1>
+    <form action="/page-2/">
+      <input type="text" />
+      <input type="submit" />
+    </form>
   </div>
 )
 
