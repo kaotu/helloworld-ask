70325c3cd30db4e5bcd4e0e85c41ca185c080220
fame <>
2018-06-19 18:54:16 +0700

    [UPDATE] Ans box

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,16 +3,21 @@ import Link from "gatsby-link"
 import styled from "styled-components"
 
 const Ans = styled.div`
-  height: 10%;
   width: 100%;
   color: #fff;
-  background-color: #000;
+  background-color: #012144;
+  border: 0.1em solid #000;
+  border-radius: 5px;
+  text-align: center;
+  margin-bottom: 1%;
 `
 
 const SecondPage = () => (
   <div>
     <h1>Question</h1>
     <Ans>HI there</Ans>
+    <Ans>HI there2</Ans>
+    <Ans>HI there3</Ans>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
