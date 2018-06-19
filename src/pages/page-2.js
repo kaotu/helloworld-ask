9f75c80e3dc4b3ac789b72a597540a3d0d1f9688
fame <>
2018-06-19 18:48:38 +0700

    [ADD] styled-components

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,18 @@
 import React from "react"
 import Link from "gatsby-link"
+import styled from "styled-components"
+
+const Ans = styled.div`
+  height: 10%;
+  width: 100%;
+  color: #fff;
+  background-color: #000;
+`
 
 const SecondPage = () => (
   <div>
     <h1>Question</h1>
-    <div />
+    <Ans>HI there</Ans>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
