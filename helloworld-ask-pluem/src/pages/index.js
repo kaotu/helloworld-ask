667982db1d66b213af542bc7c77178a79e528a45
ปลื้ม <papop.ru@mail.kmutt.ac.th>
2018-06-19 20:44:48 +0700

    [add] mobx

--- helloworld-ask-pluem/src/pages/index.js
+++ helloworld-ask-pluem/src/pages/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { Component } from 'react'
 
-const IndexPage = () => (
-  <div>
-    <h1>ASK 3.0</h1>
-    <form action="/page-2/">
-      <input type="text" />
-      <input type="submit" />
-    </form>
-  </div>
-)
+const appState = observable({
+  count: 0,
+})
+appState.increment = function() {
+  this.count++
+}
+appState.decrement = function() {
+  this.count--
+}
+
+@observer
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>ASK 3.0</h1>
+        Counter : {appState.count}
+        <br />
+        <button onClick={this.handleDec}>-</button>
+        <button onClick={this.handleInc}>+</button>
+        <form action="/page-2/">
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+  handleInc = () => {
+    appState.increment()
+  }
+  handleDec = () => {
+    appState.decrement()
+  }
+}
 
 export default IndexPage
