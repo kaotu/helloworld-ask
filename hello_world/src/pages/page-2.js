debcda575c820543e5d0c190bfd54dc290a05b3d
tinypt <pjungss@hotmail.co.th>
2018-06-19 21:12:30 +0700

    [ADD]Admin page

--- hello_world/src/pages/page-2.js
+++ hello_world/src/pages/page-2.js
@@ -3,8 +3,13 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Hi!</h1>
+    <p>Welcome to Admin page</p>
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
