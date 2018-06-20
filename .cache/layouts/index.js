522a323de9f8bdb84bbd81c3d94a87c724e56b48
fame <>
2018-06-20 21:01:58 +0700

    [REMOVE]

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/fame/Alchemist/helloworld-ask/src/layouts/index.js"
-  import data from "/Users/fame/Alchemist/helloworld-ask/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
