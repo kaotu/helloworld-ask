49a6c806361586061f7100270c1a3f6207f1c6f0
fame <>
2018-06-20 21:20:24 +0700

    [UPDATE] something

--- fame-gatsby/src/pages/index.js
+++ fame-gatsby/src/pages/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Area = styled.textarea`
+  background-color: #002d40;
+  color: #fff;
+`
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Let's Ask</h1>
+    <form action="">
+      <Area cols="100" rows="10" />
+      <br />
+      <input type="submit" />
+    </form>
+    <Link to="/page-2/">Go to Modurator</Link>
   </div>
 )
 
--- fame-gatsby/src/pages/page-2.js
+++ fame-gatsby/src/pages/page-2.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Ques = styled.div`
+  width: 100%;
+  height: 10%;
+  background-color: #4044dd;
+  color: #ffff44;
+  border: 2px solid #fff;
+  border-radius: 0.4em;
+  text-align: center;
+  padding: 0.2em;
+`
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Modurator Page</h1>
+    <h3>Question</h3>
+    <Ques>How to use this Ask</Ques>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
