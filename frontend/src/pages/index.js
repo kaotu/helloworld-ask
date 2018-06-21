fa0e3d7492941ccac1cd196af5ed2058b00f76b4
bazsup <bas10600@gmail.com>
2018-06-21 23:16:54 +0700

    [UPDATE] page/index.js - add form, textarea for enter a question - add mobx store and use it in IndexPage class

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { observable, action } from 'mobx'
+import { observer, inject } from 'mobx-react'
+import axios from 'axios'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class Question {
+  @observable question = ''
+
+  @action
+  handleChange = e => {
+    this[e.target.name] = e.target.value
+  }
+
+  @action
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log('question : ', this.question)
+    let data = await axios.post('http://localhost:3001/question', { 
+      //body
+      question: this.question
+    })
+    console.log(data)
+  }
+}
+
+const store = new Question()
+
+@observer
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        {store.question}
+        <form action="" onSubmit={store.handleSubmit}>
+          <textarea
+            name="question"
+            value={store.question}
+            onChange={store.handleChange}
+          />
+          <br />
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
