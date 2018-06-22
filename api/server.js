5fc6e7c880e25521368d8fb8b016c420dbaec4d9
tinypt <pjungss@hotmail.co.th>
2018-06-22 19:32:08 +0700

    [ADD] create server using express

---
+++ api/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const server = express()
+server.use(cors())
+server.use(bodyParser.json())
+
+server.get('/question', async (req, res) => {
+  let result = await db.select('*').from('question')
+  res.send(result)
+})
+
+server.post('/question', async (req, res) => {
+  if (req.body.question) {
+    let result = await db.insert({
+      question: req.body.question,
+      name: 'test name'
+    }).into('question')
+      .returning('*')
+    res.send({
+      message: 'server getquestion',
+      databaseRecieve: result
+    })
+  } else {
+    res.send({
+      message: 'question is not provide'
+    })
+  }
+
+})
+
+server.listen(3001, () => {
+  console.log('> Ready !')
+})
