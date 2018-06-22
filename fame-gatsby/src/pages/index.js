79d0d1a07ca663d748eb7d144b7b8a808057c1b6
fame <>
2018-06-22 21:14:52 +0700

    [UPDATE] stateless to stateful for index

--- fame-gatsby/src/pages/index.js
+++ fame-gatsby/src/pages/index.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import styled from 'styled-components'
 
-const Area = styled.textarea`
-  background-color: #002d40;
-  color: #fff;
-`
-
-const IndexPage = () => (
-  <div>
-    <h1>Let's Ask</h1>
-    <form action="">
-      <Area cols="100" rows="10" />
-      <br />
-      <input type="submit" />
-    </form>
-    <Link to="/page-2/">Go to Modurator</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Let's Ask</h1>
+        <form action="">
+          <textarea cols="100" rows="10" />
+          <br />
+          <input type="submit" />
+        </form>
+        <Link to="/page-2/">Go to Modurator</Link>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
