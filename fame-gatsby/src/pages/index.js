3d9b7ec6f5817dbefabd7d77fe1bfd79ca954c08
fame <>
2018-06-22 21:28:16 +0700

    [UPDATE] index page

--- fame-gatsby/src/pages/index.js
+++ fame-gatsby/src/pages/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
 import Link from 'gatsby-link'
+
+class Question {
+  @observable question = ''
+
+  @action
+  handleChange = e => {
+    this[e.target.name] = e.target.value
+  }
+
+  @action
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+}
 
 class IndexPage extends React.Component {
   render() {
     return (
       <div>
+        {store.question}
         <h1>Let's Ask</h1>
-        <form action="">
-          <textarea cols="100" rows="10" />
+        <form action="" onSubmit={store.handleSubmit}>
+          <textarea
+            cols="100"
+            rows="10"
+            name="question"
+            value={store.question}
+            onChange={store.handleChange}
+          />
           <br />
           <input type="submit" />
         </form>
