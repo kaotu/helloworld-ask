55981d7092d0f47a10569f20f2f7528e77d051ff
tinypt <pjungss@hotmail.co.th>
2018-06-23 11:24:54 +0700

    [UPDATE] add knex

--- api/server.js
+++ api/server.js
@@ -1,35 +1,47 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const knex = require("knex");
 
-const server = express()
-server.use(cors())
-server.use(bodyParser.json())
+const db = knex({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "ask_dev"
+  }
+});
 
-server.get('/question', async (req, res) => {
-  let result = await db.select('*').from('question')
-  res.send(result)
-})
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
 
-server.post('/question', async (req, res) => {
+server.get("/question", async (req, res) => {
+  let result = await db.select("*").from("question");
+  res.send(result);
+});
+
+server.post("/question", async (req, res) => {
   if (req.body.question) {
-    let result = await db.insert({
-      question: req.body.question,
-      name: 'test name'
-    }).into('question')
-      .returning('*')
+    let result = await db
+      .insert({
+        question: req.body.question,
+        name: "test name"
+      })
+      .into("question")
+      .returning("*");
     res.send({
-      message: 'server getquestion',
+      message: "server getquestion",
       databaseRecieve: result
-    })
+    });
   } else {
     res.send({
-      message: 'question is not provide'
-    })
+      message: "question is not provide"
+    });
   }
-
-})
+});
 
 server.listen(3001, () => {
-  console.log('> Ready !')
-})
+  console.log("> Ready !");
+});
