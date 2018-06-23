f79d92d39a332914d34a2aafc95bc57b274fbc86
Kanisorn S <firstziiz.k@gmail.com>
2018-06-23 20:19:41 +0700

    finish

--- api/server.js
+++ api/server.js
@@ -8,7 +8,44 @@ server.use(cors());
 server.use(bodyParser.json());
 
 server.get("/", (req, res) => {
-  res.send('Hi Express')
+  res.send("Hi Express!!!!");
+});
+
+let questions = [
+  {
+    text: "Hi"
+  },
+  {
+    text: "Hi"
+  }
+];
+
+server.get("/questions", (req, res) => {
+  res.json(questions);
+});
+
+server.post("/questions", (req, res) => {
+  questions = [
+    ...questions,
+    {
+      text: req.body.text
+    }
+  ];
+  res.json(questions);
+});
+
+server.get("/questions/:id", (req, res) => {
+  res.json(questions[req.params.id]);
+});
+
+server.put("/questions/:id", (req, res) => {
+  questions[req.params.id].text = req.body.text;
+  res.json(questions[req.params.id]);
+});
+
+server.delete("/questions/:id", (req, res) => {
+  questions.splice(req.params.id, 1);
+  res.send("deleted");
 });
 
 server.listen(3001, () => {
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,18 +1,53 @@
 import React from 'react'
 import Card from '../components/Card'
+import axios from 'axios'
+import { runInDebugContext } from 'vm'
 
 class IndexPage extends React.Component {
   state = {
+    questions: [],
     text: '',
   }
-  onSubmit = () => {
+
+  async componentWillMount() {
+    await this.fetchData()
+  }
+
+  fetchData = async () => {
+    let resp = await axios.get('http://localhost:3001/questions')
+
+    await this.setState({
+      questions: resp.data,
+    })
+  }
+
+  onSubmit = async () => {
     console.log('hi')
+    let resp = await axios.post('http://localhost:3001/questions', {
+      text: this.state.text,
+    })
+
+    if (resp.data) {
+      alert('Completed Yeah')
+      this.setState({
+        text: '',
+      })
+      await this.fetchData()
+    }
   }
 
   render() {
     return (
       <div>
         <div className="container pt-4">
+          <Card>
+            <ul>
+              {this.state.questions.map((question, index) => (
+                <li key={index}>{question.text}</li>
+              ))}
+            </ul>
+          </Card>
+          <br />
           <Card>
             <div className="form-group">
               <label htmlFor="">Question</label>
